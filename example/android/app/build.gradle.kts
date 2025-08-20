cc74ac278997300d3be30a65d108a942cd51361f
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2025-08-20 11:09:09 -0300

    fixing manifest

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -24,10 +24,15 @@ android {
         applicationId = "com.example.gertec_plus_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 22
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 30
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+    }
+    lint {
+        // opcional manter:
+        checkReleaseBuilds = false
+        abortOnError = false
     }
 
     buildTypes {
